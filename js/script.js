4e38df4b4073ae22a0705c46c1c86a6f6f192da0
Jessica Aliberti <jmaliberti@gmail.com>
2024-06-05 13:03:50 -0700

    Created leaning variable and associated objects, then spliced one of the items  from the object leaningGoals array.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
+const learning = {};
+
+learning.topic = "JS";
+learning.learningGoals = ["Build JS Programs","Utilize JS Properties in Portfolio Projects",
+"Understanding APIs and JSON files","Memorizing JS Common Properties"];
+learning.category = "Front End Web Development";
+learning.topicImportance = "High";
+
+console.log(learning);
+
+learning.learningGoals.splice(1, 1);
+console.log(learning);
